8134509e7f43948c6c88a5709649a1c35064be70
win-laeyee <winlae.yee@u.nus.edu>
2022-07-20 01:42:05 +0800

    Implementing help button on top bar for all pages

--- budgeking/App.js
+++ budgeking/App.js
@@ -1,7 +1,16 @@
 import * as React from "react";
+import { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Platform, StyleSheet, StatusBar, LogBox } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  StatusBar,
+  LogBox,
+  Modal,
+  View,
+  Text,
+} from "react-native";
 import colours from "./config/colours";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
@@ -21,6 +30,9 @@ import ReportsPageTable from "./pages/Report/ReportsPageTable";
 import GoalHistory from "./pages/Goal/GoalHistory";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Header } from "./config/reusableText";
+import { BlackButton } from "./config/reusableButton";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -54,12 +66,28 @@ function GoalsAll() {
       <Stack.Screen
         name="Goals"
         component={GoalsPage}
-        options={{ headerLeft: false }}
+        options={{ headerLeft: false, headerRight: () => Help("sharmaine") }}
       />
-      <Stack.Screen name="New Goal" component={NewGoal} />
-      <Stack.Screen name="Goal History" component={GoalHistory} />
-      <Stack.Screen name="Edit Goal" component={EditGoal} />
-      <Stack.Screen name="Save to Goal" component={SaveToGoal} />
+      <Stack.Screen
+        name="New Goal"
+        component={NewGoal}
+        options={{ headerRight: () => Help("sharmaine") }}
+      />
+      <Stack.Screen
+        name="Goal History"
+        component={GoalHistory}
+        options={{ headerRight: () => Help("sharmaine") }}
+      />
+      <Stack.Screen
+        name="Edit Goal"
+        component={EditGoal}
+        options={{ headerRight: () => Help("sharmaine") }}
+      />
+      <Stack.Screen
+        name="Save to Goal"
+        component={SaveToGoal}
+        options={{ headerRight: () => Help("sharmaine") }}
+      />
     </Stack.Navigator>
   );
 }
@@ -78,9 +106,13 @@ function BudgetAll() {
       <Stack.Screen
         name="Budget"
         component={BudgetPage}
-        options={{ headerLeft: false }}
+        options={{ headerLeft: false, headerRight: () => Help("sharmaine") }}
       />
-      <Stack.Screen name="Location Search" component={LocationSearch} />
+      <Stack.Screen
+        name="Location Search"
+        component={LocationSearch}
+        options={{ headerRight: () => Help("sharmaine") }}
+      />
     </Stack.Navigator>
   );
 }
@@ -99,12 +131,29 @@ function ReportsPageAll() {
       <Stack.Screen
         name="Report"
         component={ReportsPage}
-        options={{ headerLeft: false }}
+        options={{
+          headerLeft: false,
+          headerRight: () =>
+            Help(
+              "1. Overall balance shows the total savings you have. It is a difference between your all-time total expenses and income.\n\n2. Total spending shows your all-time total expenses. Upon clicking on 'TOTAL SPENDING' box, you will be taken to a 'Pie Chart View' of all your expenses that can be filtered by different time periods."
+            ),
+        }}
       />
-      <Stack.Screen name="ReportsPage" component={ReportsPage} />
-      <Stack.Screen name="Pie Chart View" component={ReportsPagePieChart} />
-      <Stack.Screen name="Custom Date" component={ChooseCustomDate} />
-      <Stack.Screen name="Table View" component={ReportsPageTable} />
+      <Stack.Screen
+        name="Pie Chart View"
+        component={ReportsPagePieChart}
+        options={{ headerRight: () => Help("me") }}
+      />
+      <Stack.Screen
+        name="Custom Date"
+        component={ChooseCustomDate}
+        options={{ headerRight: () => Help("me") }}
+      />
+      <Stack.Screen
+        name="Table View"
+        component={ReportsPageTable}
+        options={{ headerRight: () => Help("me") }}
+      />
     </Stack.Navigator>
   );
 }
@@ -123,13 +172,66 @@ function HomePageAll() {
       <Stack.Screen
         name="Home"
         component={HomePage}
-        options={{ headerLeft: false }}
+        options={{
+          headerLeft: false,
+          headerRight: () =>
+            Help(
+              "1. Choose monthly/ daily mode by clicking on the pink dropdown.\n\n2. Fill in your allowable budget in the currency textbox (beside blue progress bar) for the month or day depending on the mode you have chosen.\n\n3. Blue progress bar shows how much money you can still spend before you reach your allowable budget limit.\n\n4. Pie chart shows a breakdown of all expenses by categories for the month or day depending on the mode you have chosen.\n\n5. Last records show a more detailed view of all expenses. Upon clicking 'Show more', you will be taken to a page with a detailed view of all your records.\n\n6. Notifications will also appear on the Home Page. Upon clicking on the notification, you will be taken to the page requiring your attention."
+            ),
+        }}
       />
-      <Stack.Screen name="HomePage" component={HomePage} />
-      <Stack.Screen name="All Table View" component={AllTableView} />
+      <Stack.Screen
+        name="All Table View"
+        component={AllTableView}
+        options={{
+          headerRight: () =>
+            Help(
+              "All records are sorted in chronological order. You can toggle between red 'Expenses' button and green 'Income' button to view details of all your expenses and income of all time."
+            ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
+
+const Help = (text) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modalView}>
+          <View style={styles.modal}>
+            <Header
+              text={"\nHelp"}
+              style={{
+                fontSize: 18,
+                fontWeight: "500",
+              }}
+            />
+            <Text style={styles.modalText}>{text}</Text>
+            <BlackButton
+              text={"Okay"}
+              onPress={() => setModalVisible(!modalVisible)}
+            />
+          </View>
+        </View>
+      </Modal>
+      <TouchableOpacity onPress={() => setModalVisible(true)}>
+        <MaterialCommunityIcons
+          name="help-circle-outline"
+          size={24}
+          color="black"
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const screenOptions = (route, color) => {
   let iconName;
@@ -208,7 +310,11 @@ function MyTabs() {
         }}
       />
 
-      <Tab.Screen name="Settings" component={SettingsPage} />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsPage}
+        options={{ headerRight: () => Help("sharmaine") }}
+      />
     </Tab.Navigator>
   );
 }
@@ -242,5 +348,39 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  centeredView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 10,
+  },
+  modalView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+  },
+  modal: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    borderWidth: 1,
+    borderColor: "#000",
+  },
+  modalText: {
+    fontSize: 15,
+    marginBottom: 10,
+    fontWeight: "300",
+    textAlign: "justify",
   },
 });
