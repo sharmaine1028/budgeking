6f2e05e1690ec59e2d8de4d11ee30123e3c919d2
sharmaine1028 <sharmaine1028@gmail.com>
2022-07-20 14:47:39 +0800

    Add render no goals test for goals page

---
+++ budgeking/__tests__/goal-test.js
@@ -0,0 +1,53 @@
+import { renderNoGoals } from "../pages/Goal/GoalsPage";
+import renderer from "react-test-renderer";
+import GenerateGoal from "../pages/Goal/GenerateGoal";
+
+test("goal renders no goals yet if empty database", () => {
+  const goals = [];
+  const tree = renderer.create(
+    goals.length !== 0
+      ? goals.map((doc) => (
+          <GenerateGoal
+            key={doc.id}
+            doc={doc}
+            time={"short term"}
+            deleteItem={this.deleteGoal}
+            saveItem={this.saveToGoal}
+            editItem={this.editGoal}
+          />
+        ))
+      : renderNoGoals()
+  );
+  expect(tree).toMatchSnapshot();
+});
+
+test("goal renders correctly", () => {
+  const goals = [
+    (item = {
+      id: 0,
+      createdBy: "ywyztcPj3QcuDAahB9ZSGlBi2Ow2",
+      createdByEmail: "a@gmail.com",
+      currSavingsAmt: 5,
+      dateCreated: new Date(2022, 7, 12, 20, 37, 13),
+      deadline: new Date(2022, 7, 31, 23, 59, 59),
+      freqAmount: "0.42",
+      frequency: "Daily",
+      goalDescription: "apples",
+      isOffTrack: false,
+      isSharing: true,
+      notes: "",
+      sharingEmails: ["b@gmail.com"],
+      target: 10,
+    }),
+  ];
+  const tree = renderer
+    .create(
+      goals.length !== 0
+        ? goals.map((doc) => (
+            <GenerateGoal key={doc.id} doc={doc} time={"short term"} />
+          ))
+        : renderNoGoals()
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
