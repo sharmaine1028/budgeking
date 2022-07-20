4a66485ab9346b805c236b412a28b582b8ccbc12
sharmaine1028 <sharmaine1028@gmail.com>
2022-07-20 17:44:19 +0800

    Did minor improvement to function in generage goal by refining logic and wrapping in try catch block

--- budgeking/pages/Goal/GenerateGoal.js
+++ budgeking/pages/Goal/GenerateGoal.js
@@ -30,9 +30,10 @@ function GenerateGoal({ doc, time, deleteItem, saveItem, editItem }) {
   useEffect(() => isOffTrack(), [doc.isOffTrack]);
 
   const getPercent = () => {
-    const percent = (doc.currSavingsAmt / doc.target) * 100;
     try {
-      if (percent > 100) {
+      const percent = (doc.currSavingsAmt / doc.target) * 100;
+
+      if (percent >= 100) {
         return "100%";
       } else {
         return (doc.currSavingsAmt / doc.target) * 100 + "%";
